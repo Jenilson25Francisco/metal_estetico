a432f301e3857134d7a365d9b80e943ffe9122ad
Jenilson Francisco <franciscoviwaldo@gmail.com>
2026-07-04 21:42:24 +0100

    chore.: initial comit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return (
-    <main>
-      <div>Hello world!</div>
-    </main>
-  );
+	return (
+		<main>
+			<div>Hello world!</div>
+			<Button className="text-foreground">Click me</Button>
+		</main>
+	);
 }
