1e9721e0ccab2573491ed78c4fab8041a9bee80d
Jenilson Francisco <franciscoviwaldo@gmail.com>
2026-07-12 16:10:31 +0100

    feat: Enhance Header component with layout and styling improvements

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,20 @@
+import Image from "next/image";
+import Link from "next/link";
 import { Navlink } from "@/components/nav-links";
 
 export function Header() {
 	return (
-		<header>
-			<div>
+		<header className="absolute top-0 sticky z-10 w-full flex items-center justify-center">
+			<div className="flex items-center justify-between w-full px-5 mx-auto  sm:max-w-[1200px]">
+				<Link href="/">
+					<Image
+						src="/logo.jpg"
+						alt="Logo"
+						width={100}
+						height={100}
+						loading="eager"
+					/>
+				</Link>
 				<Navlink />
 			</div>
 		</header>
