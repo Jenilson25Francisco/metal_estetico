32496ea60b8f3231022fc8d2cc7f4678ab0f124e
Jenilson Francisco <franciscoviwaldo@gmail.com>
2026-07-20 14:22:04 +0100

    feat: Add background image to Home component and update section styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 export default function Home() {
 	return (
 		<main className="">
-			<section className="bg-[url(/background.jpg)] bg-cover bg-center min-h-screen overflow-hidden"></section>
+			<section className="bg-[url(/backgroundImage.png)] bg-cover bg-center max-h-screen overflow-hidden">
+				<div className="space-y-5 min-h-screen flex flex-col justify-center items-start mt-3 w-full px-5 mx-auto  sm:max-w-[1200px]">
+					<h1 className="font-bold text-6xl text-primary">Metal Estético</h1>
+					<p className="font-medium text-md text-accent-foreground w-2xl">
+						A harmonia perfeita entre engenharia de alta performance e a pura
+						expressão da arte em metalurgia e arquitetura de luxo
+					</p>
+				</div>
+			</section>
 		</main>
 	);
 }
